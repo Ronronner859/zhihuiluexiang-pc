957894e4388000dd84246bdd0d4fbc1c3771c053
陈毅 <ychenronron@163.com>
2024-04-01 14:49:32 +0800

    search模块的数据渲染【通过Getter进行简化数据】

--- src/store/search/index.js
+++ src/store/search/index.js
@@ -10,16 +10,28 @@ const mutations = {
     }
 }
 const actions = {
-    // 获取search模块的数据 dispatch的时候回派发一个action的传递过来的，第二个参数传递过来的至少一个空对象
-    async getSearchInfo({ commit }, params = {}) {
-        let result = await reqGetSearchInfo(params)
-        console.log(result);
-        if (result.code == 200) {
-            commit("GETSEARCHINFO", result.data)
+        // 获取search模块的数据 dispatch的时候回派发一个action的传递过来的，第二个参数传递过来的至少一个空对象
+        async getSearchInfo({ commit }, params = {}) {
+            let result = await reqGetSearchInfo(params)
+            console.log(result);
+            if (result.code == 200) {
+                commit("GETSEARCHINFO", result.data)
+            }
         }
     }
+    // 简化数据而生 计算属性 组件中要用的数据进行简化
+const getters = {
+    goodsList(state) {
+        // 返回的数据会有两种情况
+        return state.searchList.goodsList
+    },
+    trademarkList(state) {
+        return state.searchList.trademarkList
+    },
+    attrsList(state) {
+        return state.searchList.attrsList
+    }
 }
-const getters = {}
 
 export default {
     state,
